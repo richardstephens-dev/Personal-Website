6ab2a05ff1828da03ee466054ab0327dae255188
elencus <richard.stephens.15@ucl.ac.uk>
2022-12-26 17:10:31 +0000

    Experimenting with dynamically injected css

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener("click", function () {
+    // Add the rain.js script to the page
+    var script = document.createElement("script");
+    script.src = "assets/js/rain.js";
+    document.body.appendChild(script);
+});
--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,84 +1,119 @@
 styles = `
+/*
+* Hey. A white background is boring.
+* Let's start with a clean
+* working environment.
+*/
+
 pre {
-    position: fixed; width: 48 %;
-    top: 30px; bottom: 30px; left: 26 %;
+    white-space: pre-wrap;
+    position: fixed; width: 48%;
+    top: 30px; bottom: 30px; left: 26%;
     transition: left 500ms;
     overflow: auto;
-    background - color: #313744; color: #a6c3d4;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    padding: 24px 12px;
-    box - sizing: border - box;
-    border - radius: 3px;
-    box - shadow: 0px 4px 0px 2px rgba(0, 0, 0, 0.1);
+    background-color: #1e1e1e;
+    color: #d4d4d4;
+    border-left: 15px solid #454545;
+    border-top: 15px solid #454545;
+    overflow: auto;
+    /* change the style of the scrollbar */
 }
 
+/*
+* I like VSCode's dark theme.
+* Let's use that.
+*/
+pre::-webkit-scrollbar {
+    width: 15px; /* set the width of the scrollbar */
+    background-color: #1e1e1e;
+}
 
-/* 
- * Syntax highlighting 
- * Colors based on Base16 Ocean Dark
- */
+pre::-webkit-scrollbar-thumb {
+    background-color: #a6a6a6;
+    width: 15px;
+    border-radius: 0px; /* set the border radius of the thumb */
+    opacity: 0.1;
+}
 
-pre em: not(.comment) { font - style: normal; }
+pre::-webkit-scrollbar-thumb:hover {
+    background-color: #bbbbbb;
+}
 
-.comment       { color: #707e84; }
-.selector      { color: #c66c75; }
-.selector.key { color: #c66c75; }
-.key           { color: #c7ccd4; }
-.value         { color: #d5927b; }
+pre::-webkit-scrollbar-thumb:active {
+    opacity: 0.9;
+}
 
+/*
+* Ah. Now that's more like it.
+* I'm UK based, and it's probably raining here.
+*/
 
-/* 
- * Let's build my little pen heart.
- */
+.rainy-skies {
+    --tw-gradient-from: #1a1a24;
+    --tw-gradient-to: #333346;
+    --tw-gradient-stops: var(--tw-gradient-from),
+        var(--tw-gradient-to);
+    background-image: linear-gradient(to bottom,
+        var(--tw-gradient-stops));
+}
 
-
-/* First, we'll move this s*** over */
-
-pre { left: 50 %; }
-`
+/*
+* We could animate some raindrops with CSS.
+* But that's not very fun.
+* Let's use JavaScript instead.
+* We'll need another window.
+*/`
 
 var openComment = false;
-
-function writeStyleChar(which) {
+function writeStyleChar(which, textId, styleId) {
+    if (document.getElementsByClassName("blinker").length > 0) {
+        var blinker = document.getElementsByClassName("blinker")[0];
+        blinker.parentNode.removeChild(blinker);
+    }
     // begin wrapping open comments
     if (which == '/' && openComment == false) {
         openComment = true;
-        var styles = document.getElementById('style-text').innerHTML + which;
+        var styles = document.getElementById(textId).innerHTML + which;
     } else if (which == '/' && openComment == true) {
         openComment = false;
-        var styles = document.getElementById('style-text').innerHTML.replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
+        var styles = document.getElementById(textId).innerHTML.replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
     }
     // wrap style declaration
     else if (which == ':') {
-        var styles = document.getElementById('style-text').innerHTML.replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
+        var styles = document.getElementById(textId).innerHTML.replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
     }
     // wrap style value 
     else if (which == ';') {
-        var styles = document.getElementById('style-text').innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;');
+        var styles = document.getElementById(textId).innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;');
     }
     // wrap selector
     else if (which == '{') {
-        var styles = document.getElementById('style-text').innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{');
+        var styles = document.getElementById(textId).innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{');
     } else {
-        var styles = document.getElementById('style-text').innerHTML + which;
+        var styles = document.getElementById(textId).innerHTML + which;
     }
-    document.getElementById('style-text').innerHTML = styles;
-    document.getElementById('style-tag').innerHTML += which;
+    document.getElementById(textId).innerHTML = styles + "<span class='blinker'></span>";
+    document.getElementById(styleId).innerHTML += which;
 }
 
-function writeStyles(message, index, interval) {
+function writeStyles(message, index, interval, textId, styleId) {
     if (index < message.length) {
-        var pre = document.getElementById('style-text');
+        var pre = document.getElementById(textId);
         pre.scrollTop = pre.scrollHeight;
-        writeStyleChar(message[index++]);
+        writeStyleChar(message[index++], textId, styleId);
+        if (message.substring(index, index + 1).match(/\s/) && message.substring(index - 1, index).match(/\./)) {
+            interval = 100; // Pause after each sentence.
+        } else if (openComment == true) {
+            interval = 4; // Slow down comment typing.
+        } else {
+            interval = 2;
+        }
         setTimeout(function () {
-            writeStyles(message, index, interval);
+            writeStyles(message, index, interval, textId, styleId);
         }, interval);
     }
 }
 
-// appending the tags I'll need.
-document.body.innerHTML += '<style id="style-tag"></style><pre id="style-text"></pre>';
-
-// starting it off
-writeStyles(styles, 0, 20);
+let textId = 'terminal-text';
+let styleId = 'terminal-style';
+writeStyles(styles, 0, 20, textId, styleId);
