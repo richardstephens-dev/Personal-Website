8cc32598603d6a98496f4482b521bb9303d77f40
elencus <richard.stephens.15@ucl.ac.uk>
2023-01-28 11:34:25 +0000

    adjusted font sizing, hero text, minify-proof art

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 // Constants
 // Hero text
-const WELCOME_PRE_EN = `Welcome! You'll find projects here about languages and retro games. Keep in mind that the code isn't perfect! I learn by experimenting, and experiments only work when you know you can improve. If you see a way to improve a project here or on my GitHub, please contact me below.`
+const WELCOME_PRE_EN = `Welcome! You'll find projects here about languages and retro games. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
 const CONTACT_P_EN = `Richard Stephens
 richard.stephens.15@ucl.ac.uk
 +44 0 7704 930 825
@@ -9,7 +9,7 @@ const CONTACT_H1_EN = `Let's talk!`
 const PROJECTS_TITLE_EN = `Projects:`
 
 const PROJECTS_TITLE_RU = `Проекты:`
-const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты о языках и ретро-играх. Чтобы вы знали—код здесь не является идеальным! Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Если увидите способ улучшить проект здесь или на моем GitHub, пожалуйста, свяжитесь со мной.`
+const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты о языках и ретро-играх. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
 const CONTACT_P_RU = `Ричард Стивенс
 richard.stephens.15@ucl.ac.uk
 +44 0 7704 930 825
@@ -43,7 +43,6 @@ function initPage() {
     initLang();
     let localizedTexts = texts[document.cookie.split("=")[1]];
     writeBlinkerText(localizedTexts["welcome-pre"], 0, 0, "welcome-pre");
-    // Make element with projects-header id have the projects title.
     document.getElementById("projects-header").innerHTML = localizedTexts["projects-header"];
     writeProjectCards();
 }
@@ -124,6 +123,7 @@ async function writeProjectCards() {
 
     let contactDiv = document.createElement("div");
     contactDiv.classList.add("card");
+    contactDiv.id = "contact-card";
     contactDiv.innerHTML = `
         <h1>${localizedTexts["contact-h1"]}</h1>
         <p>${localizedTexts["contact-p"]}</p>
@@ -132,33 +132,30 @@ async function writeProjectCards() {
 }
 
 function toggleLang() {
-    // clear the timeout for the writeBlinkerText function.
-    // Get the current language from html. check if english or russian
     let lang = document.documentElement.getAttribute("lang");
+    clearTimeout(writeBlinkerTextTimeout);
     if (lang == "en") {
         localizedTexts = texts["ru"];
         document.documentElement.setAttribute("lang", "ru");
         document.getElementById("lang-img").src = "assets/images/en.svg";
         document.cookie = "lang=ru";
-        // stop the writeBlinkerText function.
-        clearTimeout(writeBlinkerTextTimeout);
-        // clear the welcome-pre element.
-        document.getElementById("welcome-pre").innerHTML = "";
-        // reinitialize the page.
-        initPage();
-        return;
     }
-    localizedTexts = texts["en"];
-    document.documentElement.setAttribute("lang", "en");
-    document.getElementById("lang-img").src = "assets/images/ru.svg";
-    document.cookie = "lang=en";
-    clearTimeout(writeBlinkerTextTimeout);
+    if (lang == "ru") {
+        localizedTexts = texts["en"];
+        document.documentElement.setAttribute("lang", "en");
+        document.getElementById("lang-img").src = "assets/images/ru.svg";
+        document.cookie = "lang=en";
+    }
+    document.getElementById("contact-card").innerHTML = `
+        <h1>${localizedTexts["contact-h1"]}</h1>
+        <p>${localizedTexts["contact-p"]}</p>
+    `;
+    document.getElementById("projects-header").innerHTML = localizedTexts["projects-header"];
     document.getElementById("welcome-pre").innerHTML = "";
-    initPage();
+    writeBlinkerText(localizedTexts["welcome-pre"], 0, 0, "welcome-pre");
 }
 
 function toggleTheme() {
-    // Get the current theme from html. check if dark or light
     let theme = document.documentElement.getAttribute("theme");
     if (theme == "dark") {
         document.documentElement.setAttribute("theme", "light");
@@ -172,26 +169,20 @@ function toggleTheme() {
 let writeBlinkerTextTimeout;
 function writeBlinkerText(message, index, speed, textId) {
     if (index < message.length) {
-        // Control the speed. Low value = fast.
         if (message.substring(index, index + 1).match(/\s/) && message.substring(index - 1, index).match(/[.!?]$/)) {
-            speed = 750; // Pause after each sentence.
+            speed = 750;
         } else {
-            speed = 25; // Otherwise go fast so no one loses attention span.
+            speed = 25;
         }
 
-        // Write to display element.
-        // Get rid of existing blinker.
         if (document.getElementsByClassName("blinker").length > 0) {
             var blinker = document.getElementsByClassName("blinker")[0];
             blinker.parentNode.removeChild(blinker);
         }
 
-        // Write the character to the element.
         var text = document.getElementById(textId).innerHTML + message[index++];
         document.getElementById(textId).innerHTML = text + "<span class='blinker'></span>";
 
-
-        // Invoke again.
         writeBlinkerTextTimeout = setTimeout(function () {
             writeBlinkerText(message, index, speed, textId);
         }, speed);
