42dcfa93f72bbd692f58401fb4671971d5a22488
rjslondon <rstephens@london.edu>
2023-01-30 13:55:28 +0000

    Changed .card p font size and width & contact text

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,14 @@
 // Hero text
 const WELCOME_PRE_EN = `Welcome! You'll find projects here about languages and retro games. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
 const CONTACT_P_EN = `Richard Stephens
-richard.stephens.15@ucl.ac.uk
-+44 0 7704 930 825
-London, UK`
+richard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nLondon, UK`
 const CONTACT_H1_EN = `Let's talk!`
 const PROJECTS_TITLE_EN = `Projects:`
 
 const PROJECTS_TITLE_RU = `Проекты:`
 const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты о языках и ретро-играх. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
 const CONTACT_P_RU = `Ричард Стивенс
-richard.stephens.15@ucl.ac.uk
-+44 0 7704 930 825
-Лондон, Великобритания`
+richard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nЛондон, Великобритания`
 const CONTACT_H1_RU = `Давайте поговорим!`
 
 // store the texts in a logical way.
