e1a509feddeb8d742848061d21ae3d08e7a9b3f5
elencus <richard.stephens.15@ucl.ac.uk>
2023-01-30 21:01:42 +0000

    Updated hero text.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,13 @@
 // Constants
 // Hero text
-const WELCOME_PRE_EN = `Welcome! You'll find projects here about languages and retro games. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
-const CONTACT_P_EN = `Richard Stephens
-richard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nLondon, UK`
+const WELCOME_PRE_EN = `Welcome! You'll find projects here in Python, JavaScript, and Assembly. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
+const CONTACT_P_EN = `Richard Stephens\nrichard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nLondon, UK`
 const CONTACT_H1_EN = `Let's talk!`
 const PROJECTS_TITLE_EN = `Projects:`
 
 const PROJECTS_TITLE_RU = `Проекты:`
-const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты о языках и ретро-играх. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
-const CONTACT_P_RU = `Ричард Стивенс
-richard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nЛондон, Великобритания`
+const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты на Python, JavaScript, и Assembly. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
+const CONTACT_P_RU = `Ричард Стивенс\nrichard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nЛондон, Великобритания`
 const CONTACT_H1_RU = `Давайте поговорим!`
 
 // store the texts in a logical way.
@@ -104,7 +102,7 @@ async function writeProjectCards() {
         let repoDiv = document.createElement("div");
         repoDiv.classList.add("card");
         repoDiv.innerHTML = `
-            <a href="${repo.html_url}">
+            <a href="${repo.html_url}" target="_blank">
             <h1>${repo.name.split("-").join(" ")}</h1></a>
             <p>${repo.description}
             \n(${commit_0.commit.author.date.split("T")[0]}): ${commit_0.commit.message}
