fb4fcbc708b389d1d69f7adc355737a1334cbcf0
elencus <richard.stephens.15@ucl.ac.uk>
2023-02-02 20:50:43 +0000

    Added code icons and made hero text simpler.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 // Constants
 // Hero text
-const WELCOME_PRE_EN = `Welcome! You'll find projects here in Python, JavaScript, and Assembly. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
+const WELCOME_PRE_EN = `Welcome! You'll find coding projects here. Keep in mind that the code isn't perfect. I learn by experimenting, and experiments only work when you know you can improve. When you see a way to improve a project, let me know!`
 const CONTACT_P_EN = `Richard Stephens\nrichard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nLondon, UK`
 const CONTACT_H1_EN = `Let's talk!`
 const PROJECTS_TITLE_EN = `Projects:`
 
 const PROJECTS_TITLE_RU = `Проекты:`
-const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты на Python, JavaScript, и Assembly. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
+const WELCOME_PRE_RU = `Добро пожаловать! Здесь вы найдете проекты программирования. Чтобы вы знали—код здесь не является идеальным. Я учусь на экспериментах, и это возможно только тогда, когда знаете, можете улучшить. Когда увидите способ улучшить проект, дайте мне знать!`
 const CONTACT_P_RU = `Ричард Стивенс\nrichard.stephens.15@ucl.ac.uk\n+44 0 7704 930 825\nЛондон, Великобритания`
 const CONTACT_H1_RU = `Давайте поговорим!`
 
@@ -78,26 +78,50 @@ async function writeProjectCards() {
     }
 
     let repo_commits = {};
+    let repo_langs = {};
     for (let i = 0; i < repo_names.length; i++) {
         const commits_result = await fetch(`https://api.github.com/repos/richardstephens-dev/${repo_names[i]}/commits?per_page=3`)
             .then(commits_response => commits_response.json())
             .then(data => {
                 return JSON.stringify(data, null, 2);
             });
+        const langs_result = await fetch(`https://api.github.com/repos/richardstephens-dev/${repo_names[i]}/languages`)
+            .then(langs_response => langs_response.json())
+            .then(data => {
+                return JSON.stringify(data, null, 2);
+            });
 
         let commits = JSON.parse(commits_result);
         repo_commits[repo_names[i]] = commits;
+        let langs = JSON.parse(langs_result);
+        repo_langs[repo_names[i]] = langs;
     }
 
     let projectCards = document.getElementById("project-cards");
     projectCards.innerHTML = "";
     for (let i = 0; i < Math.min(5, repos.length); i++) {
-        let commit_0 = repo_commits[repos[i].name][0];
-        let commit_1 = repo_commits[repos[i].name][1];
-        let commit_2 = repo_commits[repos[i].name][2];
-        if (commit_0 == undefined || commit_1 == undefined || commit_2 == undefined) {
-            continue;
+        let commit_html = "";
+        let langs_html = `<div class="langs-wrapper">`;
+        for (let j = 0; j <= 3; j++) {
+            let commit = repo_commits[repos[i].name][j];
+            if (commit == undefined) {
+                continue;
+            }
+            commit_html += `(\n${commit.commit.author.date.split("T")[0]}): ${commit.commit.message}<br>`;
         }
+        for (let lang in repo_langs[repos[i].name]) {
+            // If the src file doesn't exist, don't add it. check using xmlhttprequest.
+            src = `assets/images/${lang}.svg`;
+            let req = new XMLHttpRequest();
+            req.open("GET", src, false);
+            req.send();
+            if (req.status == 404) {
+                continue;
+            }
+            langs_html += `<img class="logo" src="${src}"/>`;
+        }
+        langs_html += `</div>`;
+
         let repo = repos[i];
         let repoDiv = document.createElement("div");
         repoDiv.classList.add("card");
@@ -105,9 +129,8 @@ async function writeProjectCards() {
             <a href="${repo.html_url}" target="_blank">
             <h1>${repo.name.split("-").join(" ")}</h1></a>
             <p>${repo.description}
-            \n(${commit_0.commit.author.date.split("T")[0]}): ${commit_0.commit.message}
-            \n(${commit_1.commit.author.date.split("T")[0]}): ${commit_1.commit.message}
-            \n(${commit_2.commit.author.date.split("T")[0]}): ${commit_2.commit.message}
+            ${commit_html}
+            ${langs_html}
             </p>
         `;
         repoDiv.style.top = `${i * 10}px`;
